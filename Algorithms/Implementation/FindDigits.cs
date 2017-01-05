f604123de89a069741b63060fa743efb4fee1e57
AntyfrizZz <iavor.milinov@gmail.com>
2017-01-05 20:07:31 +0200

    FindDigits

---
+++ Algorithms/Implementation/FindDigits.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Implementation
+{
+    public static class FindDigits
+    {
+        public static void Solve()
+        {
+            var numberOfTests = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfTests; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+                var savedNumber = number;
+                var result = 0;
+
+                do
+                {
+                    var digit = number % 10;
+
+                    if (digit != 0 && savedNumber % digit == 0)
+                    {
+                        result++;
+                    }
+
+                    number /= 10;
+                } while (number != 0);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
