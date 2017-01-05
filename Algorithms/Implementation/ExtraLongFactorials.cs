038d2bc8cd99fe33b31ed398285ec0e9fb46cea9
AntyfrizZz <iavor.milinov@gmail.com>
2017-01-05 21:02:23 +0200

    ExtraLongFactorials

---
+++ Algorithms/Implementation/ExtraLongFactorials.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace Implementation
+{
+    public static class ExtraLongFactorials
+    {
+        public static void Solve()
+        {
+            var n = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+            while (n != 1)
+            {
+                result *= n;
+                n -= 1;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
