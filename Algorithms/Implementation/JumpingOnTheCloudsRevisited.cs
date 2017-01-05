3389d79e1c503c3b5f81296bd55498448abd59da
AntyfrizZz <iavor.milinov@gmail.com>
2017-01-05 19:26:18 +0200

    Jumping On The Clouds Revisited

---
+++ Algorithms/Implementation/JumpingOnTheCloudsRevisited.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Implementation
+{
+    public static class JumpingOnTheCloudsRevisited
+    {
+        public static void Solve()
+        {
+            var tockens = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var n = tockens[0];
+            var k = tockens[1];
+
+            var clouds = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var index = 0;
+            var energy = 100;
+
+            do
+            {
+                index = (index + k) % n;
+                energy -= clouds[index] == 1 ? 3 : 1;
+
+            } while (index != 0);
+
+            Console.WriteLine(energy);
+        }
+    }
+}
